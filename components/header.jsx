6e2bd06c40f8bb3e4a8015f86cdf7c5549d6dd51
Zannatul08 <ferdous89001@gmail.com>
2025-07-03 02:15:47 +0600

    Made minor changes in logo

--- components/header.jsx
+++ components/header.jsx
@@ -40,81 +40,81 @@ const Header = () => {
 
                             </Button>
                         </Link>
-                    
 
-                    <DropdownMenu>
-                        <DropdownMenuTrigger>
-                            <Button>
-                                <StarsIcon className='h-4 w-4' />
-                                <span className='hidden md:block'>
-                                    Growth Tools 
-                                </span>
-                                <ChevronDown className='h-4 w-4'/>
 
-                            </Button>
-                        </DropdownMenuTrigger>
-                        <DropdownMenuContent>
+                        <DropdownMenu>
+                            <DropdownMenuTrigger>
+                                <Button>
+                                    <StarsIcon className='h-4 w-4' />
+                                    <span className='hidden md:block'>
+                                        Growth Tools
+                                    </span>
+                                    <ChevronDown className='h-4 w-4' />
 
-                            <DropdownMenuItem>
-                                <Link href={"/resume"} className='flex items-center gap-2'>
-                                <FileText className='h-4 w-4' />
-                                <span >
-                                   Build Resume 
-                                </span>
-                                </Link>
-                            </DropdownMenuItem>
-                            <DropdownMenuItem>
-                                <Link href={"/ai-cover-letter"} className='flex items-center gap-2'>
-                                <PenBox className='h-4 w-4' />
-                                <span >
-                                   Cover Letter 
-                                </span>
-                                </Link>
+                                </Button>
+                            </DropdownMenuTrigger>
+                            <DropdownMenuContent>
+
+                                <DropdownMenuItem>
+                                    <Link href={"/resume"} className='flex items-center gap-2'>
+                                        <FileText className='h-4 w-4' />
+                                        <span >
+                                            Build Resume
+                                        </span>
+                                    </Link>
                                 </DropdownMenuItem>
-                            <DropdownMenuItem>
-                                <Link href={"/interview"} className='flex items-center gap-2'>
-                                <GraduationCap className='h-4 w-4' />
-                                <span >
-                                  Interview Prep 
-                                </span>
-                                </Link>
+                                <DropdownMenuItem>
+                                    <Link href={"/ai-cover-letter"} className='flex items-center gap-2'>
+                                        <PenBox className='h-4 w-4' />
+                                        <span >
+                                            Cover Letter
+                                        </span>
+                                    </Link>
+                                </DropdownMenuItem>
+                                <DropdownMenuItem>
+                                    <Link href={"/interview"} className='flex items-center gap-2'>
+                                        <GraduationCap className='h-4 w-4' />
+                                        <span >
+                                            Interview Prep
+                                        </span>
+                                    </Link>
                                 </DropdownMenuItem>
 
-                        </DropdownMenuContent>
-                    </DropdownMenu>
+                            </DropdownMenuContent>
+                        </DropdownMenu>
                     </SignedIn>
- <SignedOut>
-    <SignInButton>
-        <Button variant="outline">
-            Sign In
-        </Button>
-    </SignInButton>
-</SignedOut>
-<SignedIn>
- <UserButton 
-   appearance={{
-    elements:{
-        avatarBox:"w-10 h-10",
-        userButtonPopoverCard: "shadow-xl",
-        userPreviewMainIdentifier:"font-semibold",
-    },
-   }}
- afterSignOutUrl='/'
- />
- </SignedIn>
+                    <SignedOut>
+                        <SignInButton>
+                            <Button variant="outline">
+                                Sign In
+                            </Button>
+                        </SignInButton>
+                    </SignedOut>
+                    <SignedIn>
+                        <UserButton
+                            appearance={{
+                                elements: {
+                                    avatarBox: "w-10 h-10",
+                                    userButtonPopoverCard: "shadow-xl",
+                                    userPreviewMainIdentifier: "font-semibold",
+                                },
+                            }}
+                            afterSignOutUrl='/'
+                        />
+                    </SignedIn>
                 </div>
             </nav>
-            
-                
-                {/* <SignUpButton>
+
+
+            {/* <SignUpButton>
                     <button className="bg-[#6c47ff] text-white rounded-full font-medium text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5 cursor-pointer">
                         Sign Up
                     </button>
                 </SignUpButton>
              */}
-            
-               
-            
+
+
+
         </header>
     )
 }
