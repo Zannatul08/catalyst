6206be0475db9ffdbd09c5ae3c09f73c45fa669e
MMaria18 <mhmaria027@gmail.com>
2025-07-06 20:46:13 +0600

    Landing page

--- components/ui/hero.jsx
+++ components/ui/hero.jsx
@@ -1,23 +1,49 @@
+"use client";
+
 import Link from "next/link";
 import { Button } from "./button";
 import Image from "next/image";
+import { useEffect, useRef } from "react";
 
 const HeroSection = () => {
+  
+  const imageRef = useRef(null);
+  useEffect(()=>{
+    const imageElement = imageRef.current;
+    const handleScroll = () =>{
+       const scrollPosition = window.scrollY;
+    const scrollThreshold =100;
+
+    if(scrollPosition > scrollThreshold){
+      imageElement.classList.add("scrolled");
+    }
+    else{
+      imageElement.classList.remove("scrolled")
+    }
+
+    };
+
+    window.addEventListener("scroll",handleScroll);
+ 
+    return () => window.removeEventListener("scroll", handleScroll);
+   
+  } , []);
+
   return (
     <section className="w-full pt-36 md:pt-48 pb-10">
       <div className="space-y-6 text-center">
         <div className="space-y-6 mx-auto">
-          <h1>
+          <h1 className="gradient-title text-5xl font-bold md:text-6xl lg:text-7xl xl:text-8xl">
             Your AI Career Coach for
             <br />
             Professional Success
           </h1>
-          <p>
+          <p className="mx-auto max-w-[600px] text-muted-foreground md:text-xl">
             Advance your career with personalized guidance,interview prep, and
             AI-powered tools for job success.
           </p>
         </div>
-        <div>
+        <div className="flex justify-center space-x-4">
           <Link href="/dashboard">
             <Button size="lg" className="px-8">
               Get Started
@@ -29,8 +55,8 @@ const HeroSection = () => {
             </Button>
           </Link>
         </div>
-        <div>
-          <div className="">
+        <div className="hero-image-wrapper mt-5 md:mt-0">
+          <div ref={imageRef} className="hero-image">
             <Image
               src={"/banner.jpeg"}
               width={1080}
