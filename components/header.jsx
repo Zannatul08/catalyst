9b5ca24da53d810975cc040f79d7b7c7c07bfc64
Anik <arafatanik92@gmail.com>
2025-07-08 16:01:09 +0600

    Fixed user creation logic and Clerk integration

--- components/header.jsx
+++ components/header.jsx
@@ -14,11 +14,12 @@ import {
     DropdownMenu,
     DropdownMenuContent,
     DropdownMenuItem,
-    DropdownMenuLabel,
-    DropdownMenuSeparator,
+    
     DropdownMenuTrigger,
 } from "@/components/ui/dropdown-menu"
-const Header = () => {
+import { checkUser } from '@/lib/checkUser'
+const Header =async () => {
+    await checkUser()
     return (
         <header className='fixed top-0 w-full border-b bg-background/80 backdrop-blur-md z-50 supports-[backdrop-filter]:bg-background/60 '>
 
