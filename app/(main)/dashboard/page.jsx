096883c11388e769a75878f9e79b17b81ef2b524
labiba4040 <labibahabib10@gmail.com>
2025-07-14 18:01:59 +0600

    insight page cards done

--- app/(main)/dashboard/page.jsx
+++ app/(main)/dashboard/page.jsx
@@ -1,6 +1,8 @@
 import { currentUser } from "@clerk/nextjs/server";
 import { getUserOnboardingStatus } from "@/actions/user";
 import { redirect } from "next/navigation";
+import { getIndustryInsights } from "@/actions/dashboard";
+import DashboardView from "./_components/dashboard-view";
 
 const IndustryInsightsPage = async () => {
   const user = await currentUser();
@@ -9,13 +11,18 @@ const IndustryInsightsPage = async () => {
     return <div className="text-red-500 text-center mt-20">User not found</div>;
   }
 
-  const { isOnboarded } = await getUserOnboardingStatus(user.id);
+  const { isOnboarded } = await getUserOnboardingStatus();
+  const insights = await getIndustryInsights();
 
   if (!isOnboarded) {
     redirect("/onboarding");
   }
 
-  return <div>IndustryInsightsPage</div>;
+  return (
+  <div className="container mx-auto">
+    <DashboardView insights={insights}/>
+  </div>
+  );
 
 };
 
